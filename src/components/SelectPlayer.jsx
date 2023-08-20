94785b7271161238a9052f5e991859095c483cda
Adekunle Adesokan <kunle.adesokan9@gmail.com>
2023-08-20 09:06:28 +0200

    first commit

---
+++ src/components/SelectPlayer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+function SelectPlayer() {
+  return <div></div>;
+}
+export default SelectPlayer;
---
+++ src/components/Start.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Start(props) {
+  return <div></div>;
+}
+
+export default Start;
